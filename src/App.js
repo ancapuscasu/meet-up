0eaa94a85eb726e8b6a31d2047b81a0e059f9296
Anca Puscasu <ancapuscasu@ymail.com>
2022-01-31 20:04:48 -0500

    imported EventList, CitySearch and NumberOfEvents into App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+
+//components
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <EventList />
+        <NumberOfEvents />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/** 
 import React from 'react';
-import logo from './logo.svg';
+
+//components
+import EventList from './EventList';
+
+//styling
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EventList />
     </div>
   );
 }
 
 export default App;
+
+*/
+
