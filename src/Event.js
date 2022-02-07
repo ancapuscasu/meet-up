0d8dcc3fce50feb0d45eb1b82cf8b781b7271e2c
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-07 13:41:03 -0500

    handleDetailsClick function on event

--- src/Event.js
+++ src/Event.js
@@ -13,7 +13,7 @@ class Event extends Component {
     const current = this.state.collapsed;
     this.setState({
       collapsed: current ? false : true,
-      eventDetailsButtonText: current ? "More details" : "Hide details"
+      eventDetailsButtonText: current ? "Hide details" : "More details" 
     });
   }
 
@@ -25,24 +25,23 @@ class Event extends Component {
     return (
     <div className="event">
       <h3 className="summary">{event.summary}</h3>
-      <p> <span className="start-time">{event.start.dateTime}</span></p>
+      <p> <span className="start-time">{event.start.dateTime} ({event.start.timeZone})</span></p>
+      <p className="event-details__location">{event.location}</p>
 
+      
       {!collapsed && (
         <div className="event-details">
-          <h3 className="event-details__summary">{event.summary}</h3>
           <p className="event-details__description">{event.description}</p>
           <p> <span className="event-details__start-time">{event.start.dateTime}</span>-<span className="event-details__end-time">{event.end.dateTime}</span></p>
-          <p className="event-details__location">{event.location}</p>
           <p>View event on Google Calendar 
             <a className="event-details__link" href={event.htmlLink}>{event.htmlLink}</a>
           </p>
         </div>
       )}
 
-      <button 
-        className="show-details" 
-        onClick={this.handleDetailsClick}
-        >{eventDetailsButtonText}</button>
+      <button className="details-btn" onClick={this.handleDetailsClick}>
+        {eventDetailsButtonText}
+      </button>
     </div>
     )
   }
