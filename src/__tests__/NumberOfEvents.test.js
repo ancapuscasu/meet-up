dde4c856316013000788cb2b8792e036834afb50
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-07 13:37:12 -0500

    NumberOfEvents unit testing

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,7 +5,7 @@ import NumberOfEvents from "../NumberOfEvents";
 describe('<NumberOfEvents /> component', () => {
     let NumberOfEventsWrapper;
     beforeAll(() => {
-        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+        NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}}/>);
     });
 
     test('render number of events element', () =>{
@@ -16,17 +16,13 @@ describe('<NumberOfEvents /> component', () => {
         expect(NumberOfEventsWrapper.find('.number-of-events__input')).toHaveLength(1);
     })
 
-    test('renders number of events correctly', () => {
-        const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
-        expect(NumberOfEventsWrapper.find('.number-of-events__input').get(0).props.value).toBe(numberOfEvents);
-    })
-
     test('change state.numberOfEvents when text input changes', () => {
-        NumberOfEventsWrapper.setState({
-            numberOfEvents: '32'
-        });
-        const eventObject = { target: { value: '12'}};
+        // NumberOfEventsWrapper.setState({
+        //     NumberOfEvents: 32
+        //   });
+        const eventObject = { target: { value: 12}};
         NumberOfEventsWrapper.find('.number-of-events__input').simulate('change', eventObject);
-        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe('12');
+        expect(NumberOfEventsWrapper.state("newNumber")).toBe(12);
     });
 });
+
