ea0b4c739e7ef5c3c611c1a17d39077f9a848c18
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-10 18:46:45 -0500

    Added end-to-end testing for feature 1

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,6 +1,54 @@
 import puppeteer from 'puppeteer';
+import mockData from '../mock-data';
 
-describe('It must be able to show/hide an events details', () => {
+describe('filter events by city (feature 1)', () => {
+
+    let browser;
+    let page; 
+    jest.setTimeout(30000);
+
+    beforeAll(async () => {
+    
+        browser = await puppeteer.launch({
+            // headless: false,
+            slowMo: 250,
+            ignoreDefaultArgs: ['--disable-extensions']
+        });
+        page = await browser.newPage();
+
+        await page.goto('http://localhost:3000/');
+    });
+
+    afterAll(async () => {
+        browser.close();
+    });
+
+
+    test('When user hasn’t searched for a city, show upcoming events from all cities', async () => {
+        const eventListLength = await page.$$eval('.event', events => events.length);
+        expect(eventListLength).toBe(6);
+    });
+
+
+    test('User should see a list of suggestions when they search for a city', async () => {
+        await page.type('.city', 'Berlin', {delay: 75});
+        const suggestionListLength = await page.$$eval('.suggestions li', suggestions => suggestions.length);
+        expect(suggestionListLength).toBe(2);
+    });
+
+
+    test('User can select a city from the suggested list', async () => {
+        await page.click('.suggestions li');
+
+        const eventListLength = await page.$$eval('.event', events => events.length);
+        expect(eventListLength).toBe(3);
+    });
+
+});
+
+
+
+describe('It must be able to show/hide an events details (feature 2)', () => {
     
     let browser;
     let page; 
@@ -43,3 +91,4 @@ describe('It must be able to show/hide an events details', () => {
 
     })
 });
+
