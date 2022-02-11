8464f9a52495d40f45c9ecc7eb880363b54cfc1a
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-10 19:47:44 -0500

    installed atatus-spa package and imported config line in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+//atatus
+import * as atatus from 'atatus-spa';
+atatus.config('0bf88adef669485e980f2abf3844e15a').install();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -21,3 +25,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
