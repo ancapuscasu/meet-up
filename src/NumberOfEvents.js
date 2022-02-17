f329fbadab262727f57a3eeba71ec7de89d73c74
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-17 14:37:46 -0500

    added errorAlert to NumberOfEvents to deal with numbers <1 || >25

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   
+  state = {
+    infoText: '',
+    newNumber: this.props.numberOfEvents
+  }
 
+  handleInputChange = async (e) => {
+    const number = parseInt(e.target.value);
+    if (number < 1 || number > 25 || isNaN(number)) {
+      this.setState({
+        infoText: 'Please choose a number between 1 and 25',
+        newNumber: number
 
-  handleInputChange = (e) => {
-    const newNumber = parseInt(e.target.value);
-    this.props.updateNumberOfEvents(newNumber);
+      })
+    } else {
+      await this.setState({
+        infoText: '',
+        newNumber: number
+      })
+      this.props.updateNumberOfEvents(this.state.newNumber);
+    }
   } 
 
 
 
   render() {
-    const { numberOfEvents } = this.props;
+    const { newNumber, infoText } = this.state;
 
     return (
-      <div className='number-of-events'>
-        <label className="number-of-events__label">Number of events:</label>
-        <input
-          type="number"
-          className="number-of-events__input"
-          value={numberOfEvents}
-          onChange={(e) => this.handleInputChange(e)}
-        />
+      <div className='number-of-events-container'>
+        <ErrorAlert text={infoText} />
+        <div className='number-of-events'>
+          <label className="number-of-events__label">Number of events:</label>
+          <input
+            type="number"
+            className="number-of-events__input"
+            value={newNumber}
+            onChange={(e) => this.handleInputChange(e)}
+          />
+        </div>
       </div>
     );
   }
