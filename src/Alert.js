843170a63a103f16d9dfcdd60416fe2a9ab223d4
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-17 19:06:39 -0500

    changed styling to alerts

--- src/Alert.js
+++ src/Alert.js
@@ -4,8 +4,8 @@ class Alert extends Component {
     constructor(props) {
         super(props);
         this.color = null;
-        this.width = '20rem';
-        this.fontSize = '0.8rem';
+        this.width = null;
+        this.fontSize = null;
         this.margin = "0.5rem auto";
     }
 
@@ -14,7 +14,7 @@ class Alert extends Component {
         return {
             color: this.color,
             width: this.width,
-            "font-size": this.fontSize,
+            'font-size': this.fontSize,
             margin: this.margin
         };
     }
@@ -33,6 +33,8 @@ class Alert extends Component {
     constructor(props) {
         super(props);
         this.color = '#29d2e4';
+        this.width = '20rem';
+        this.fontSize = '0.8rem';
     }
 }
 
@@ -40,6 +42,8 @@ export class ErrorAlert extends Alert {
     constructor(props) {
         super(props);
         this.color = '#e91e63';
+        this.width = '20rem';
+        this.fontSize = '0.8rem';
     }
 }
 
@@ -47,6 +51,8 @@ export class WarningAlert extends Alert {
     constructor(props) {
         super(props);
         this.color= '#ff9933';
+        this.width = '100%';
+        this.fontSize = '1.2rem';
     }
 
 }
