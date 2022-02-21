1e9d07c62d73c401160a48aeb22cfdba9fd5b2e4
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-21 17:33:18 -0500

    Added data vis components and changed componentDidMount()

--- src/App.js
+++ src/App.js
@@ -9,46 +9,58 @@ import NumberOfEvents from './NumberOfEvents';
 import { checkToken, extractLocations, getAccessToken, getEvents } from './api';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import EventGenre from './EventGenre';
+import EventsPerCityPlot from './EventsPerCityPlot';
 
 class App extends Component {
   
   state = {
     events:[],
+    eventsOnPage:[],
     locations:[],
     currentLocation: 'all',
     numberOfEvents: 12,
-    offlineAlertText: '',
     showWelcomeScreen: undefined
   }
 
   async componentDidMount() {
     this.mounted = true;
 
-    // const accessToken = localStorage.getItem('access_token');
-    // const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    // const searchParams = new URLSearchParams(window.location.search);
-    // const code = searchParams.get("code");
-    // this.setState({ 
-    //   showWelcomeScreen: !(code || isTokenValid) 
-    // });
-    // if ((code || isTokenValid) && this.mounted) {
+    if ((localStorage.getItem('access_token') !== null) && (!navigator.onLine)) {
       getEvents().then((events) => {
         if (this.mounted) {
           this.setState({ 
-            events: events.slice(0, this.state.numberOfEvents),
+            events: events,
+            eventsOnPage: events.slice(0, this.state.numberOfEvents),
             locations:extractLocations(events), 
+            showWelcomeScreen: false
           });
         }
-      }); 
+      });
+    } else {
+      const accessToken = localStorage.getItem('access_token');
+      const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = searchParams.get("code");
+      this.setState({ 
+        showWelcomeScreen: !(code || isTokenValid) 
+      });
+      if ((code || isTokenValid) && this.mounted) {
+        getEvents().then((events) => {
+          if (this.mounted) {
+            this.setState({ 
+              events: events,
+              eventsOnPage: events.slice(0, this.state.numberOfEvents),
+              locations:extractLocations(events), 
+            });
+          }
+        }); 
+      }
     }
-  // }
+  }
 
   componentWillUnmount() {
     this.mounted = false;
-  }
-
-  componentDidUpdate() {
-    this.offlineAlert = () => {};
   }
 
 
@@ -71,66 +83,48 @@ class App extends Component {
       if(this.mounted) {
         const shownEvents = locationEvents.slice(0, numberOfEvents);
         this.setState({
-            events: shownEvents,
+            eventsOnPage: shownEvents,
             currentLocation: location
         });
       }
     });
   }
 
-  handlePageClick = event => {
-    const selectedPage = event.selected;
-    const offset = selectedPage * this.state.perPage;
-
-    this.setState({
-      currentPage: selectedPage,
-      offset: offset
-    }, () => {
-      this.receivedData()
-    })
-  }
-
-  offlineAlert() {
-    if (!navigator.onLine) {
-      this.setState({
-        offlineAlertText: 'You are not connected to the internet. The events you see may not be up-to-date.'
-      });
-    } else {
-      this.setState({
-        offlineAlertText: ''
-      });
-    }
-  }
-
 
   render() {
-    let { events, locations, numberOfEvents, offlineAlertText, showWelcomeScreen } = this.state;
+    let { eventsOnPage, events, locations, numberOfEvents, showWelcomeScreen } = this.state;
 
     if (showWelcomeScreen === undefined) return <div className='App' />
 
-    if(events.length === 0) {
       return (
         <div className="App">
-          <WarningAlert text = {offlineAlertText} />
-          <CitySearch locations={locations} updateEvents={this.updateEvents}/>
-          <NumberOfEvents numberOfEvents={numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents}/>
-          <div class="lds-dual-ring"></div>
-          <WelcomeScreen showWelcomeScreen={showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
+          { !navigator.onLine ? <WarningAlert text='You are not connected to the internet. The events you see may not be up-to-date.'/> : <WarningAlert text=''/>}
+          <h1>Meet Up App</h1>
+
+          <CitySearch 
+            locations={locations} 
+            updateEvents={this.updateEvents}
+          />
+          <NumberOfEvents 
+            numberOfEvents={numberOfEvents} 
+            updateNumberOfEvents={this.updateNumberOfEvents}
+          />
+      
+          <div className='data-vis-wrapper'>
+            <EventGenre events={events} />
+            <EventsPerCityPlot locations={locations} events={events}/>
+          </div>
+
+          {/* //EventList// */}
+          {events.length === 0 ? <div class="lds-dual-ring"></div> : <EventList events={eventsOnPage}/> }
+
+          <WelcomeScreen 
+            showWelcomeScreen={showWelcomeScreen} 
+            getAccessToken={() => { getAccessToken() }}
+          />
        </div>
       );
-    } else {
-      return (
-        <div className="App">
-          <WarningAlert text = {offlineAlertText} />
-          <CitySearch locations={locations} updateEvents={this.updateEvents}/>
-          <NumberOfEvents numberOfEvents={numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents}/>
-          <EventList events={events}/>
-          <WelcomeScreen showWelcomeScreen={showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
-        </div>
-      );
-    }
-    
-  }
+  } 
 }
 
 export default App;
