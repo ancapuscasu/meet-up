bc69b46fb3dbecfb52ff1ffd3fb7f447f0d86124
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-22 16:56:17 -0500

    changed EventsPerCityPlot to functional component

--- src/EventsPerCityPlot.jsx
+++ src/EventsPerCityPlot.jsx
@@ -1,58 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
-class EventsPerCityPlot extends Component { 
+const EventsPerCityPlot = ({locations, events }) => {
+  
+  const [ data, setData ] = useState([]);
+  useEffect(() => {
+    const getData = () => {
+      const data = locations.map((location)=>{
+        const number = events.filter((event) => event.location === location).length
+        let city;
+          if (location.includes('NSW')){
+            city = location.split('NSW').shift();
+          } else if (location.includes(', ')) {
+            city = location.split(', ').shift();
+          } else if (location.includes('- ')) {
+              city = location.split('- ').shift();
+          } 
+        return {city, number};
+      })
+      return data;
+    };
+    setData(() => getData());
+  }, [locations, events]);
 
-  getData = () => {
-    const {locations, events} = this.props;
-    const data = locations.map((location)=>{
-      const number = events.filter((event) => event.location === location).length
-      let city;
-        if (location.includes('NSW')){
-          city = location.split('NSW').shift();
-        } else if (location.includes(', ')) {
-          city = location.split(', ').shift();
-        } else if (location.includes('- ')) {
-            city = location.split('- ').shift();
-        } 
-      return {city, number};
-    })
-    return data;
-  };
+    
 
-  render() {
-
-    return (
-      <div className='data-vis-wrapper__plot'>
-        <h4>Events in each city</h4>
-        <ResponsiveContainer height={250}>
-          <ScatterChart
-            margin={{ 
-              top: 20, right: 20, bottom: 20, left: 0 
-            }}
-          >
-            <CartesianGrid />
-            <XAxis 
-              type="category" 
-              dataKey="city" 
-              name="City" 
-              minTickGap = "2"
-              tickMargin = "10"
-            />
-            <YAxis 
-              type="number" 
-              dataKey="number" 
-              name="Number Of Events" 
-              allowDecimals={false}
-            />
-            <Tooltip labelFormatter={() => ""} cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
-      </div>
-    )
-  }
+  return (
+    <div className='data-vis-wrapper__plot'>
+      <h4>Events in each city</h4>
+      <ResponsiveContainer height={250}>
+        <ScatterChart
+          margin={{ 
+            top: 20, right: 20, bottom: 20, left: 0 
+          }}
+        >
+          <CartesianGrid />
+          <XAxis 
+            type="category" 
+            dataKey="city" 
+            name="City" 
+            minTickGap = "2"
+            tickMargin = "10"
+          />
+          <YAxis 
+            type="number" 
+            dataKey="number" 
+            name="Number Of Events" 
+            allowDecimals={false}
+          />
+          <Tooltip labelFormatter={() => ""} cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={data} fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
 export default EventsPerCityPlot;
